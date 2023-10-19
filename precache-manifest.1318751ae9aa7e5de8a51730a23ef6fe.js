b04c59edc379f404c8f5a90a1c9218de63ab88fb
Code Sandbox <hello@codesandbox.io>
2023-10-19 19:16:53 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1318751ae9aa7e5de8a51730a23ef6fe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15f85e08af8cd087fd8a44faf5c0c2d",
+    "url": "https://edwardgazitonline.github.io/csb-w9zxt6/index.html"
+  },
+  {
+    "revision": "7cbd2568c63a70ffe2e5",
+    "url": "https://edwardgazitonline.github.io/csb-w9zxt6/static/js/2.ab26a633.chunk.js"
+  },
+  {
+    "revision": "48e5bdaa767f39f0cb05",
+    "url": "https://edwardgazitonline.github.io/csb-w9zxt6/static/js/main.d5c2845d.chunk.js"
+  },
+  {
+    "revision": "e767e878be85fac5642a",
+    "url": "https://edwardgazitonline.github.io/csb-w9zxt6/static/js/runtime~main.43dbeb35.js"
+  }
+]);
